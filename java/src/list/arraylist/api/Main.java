24ac58050e742cd966b0ad9087040fb921443219
jjeom87 <jjeom87@gmail.com>
2018-03-28 21:05:00 +0900

    fix up

--- java/src/list/arraylist/api/Main.java
+++ java/src/list/arraylist/api/Main.java
@@ -5,54 +5,54 @@ import java.util.Iterator;
 
 public class Main {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		ArrayList<Integer> numbers = new ArrayList<>();
+    ArrayList<Integer> numbers = new ArrayList<>();
 
-		numbers.add(10);
-		numbers.add(20);
-		numbers.add(30);
-		numbers.add(40);
-		System.out.println("add(°ª)");
-		System.out.println(numbers);
+    numbers.add(10);
+    numbers.add(20);
+    numbers.add(30);
+    numbers.add(40);
+    System.out.println("add(ê°’)");
+    System.out.println(numbers);
 
-		numbers.add(1, 50);
-		System.out.println("\nadd(ÀÎµ¦½º, °ª)");
-		System.out.println(numbers);
+    numbers.add(1, 50);
+    System.out.println("\nadd(ì¸ë±ìŠ¤, ê°’)");
+    System.out.println(numbers);
 
-		numbers.remove(2);
-		System.out.println("\nremove(ÀÎµ¦½º)");
-		System.out.println(numbers);
+    numbers.remove(2);
+    System.out.println("\nremove(ì¸ë±ìŠ¤)");
+    System.out.println(numbers);
 
-		System.out.println("\nget(ÀÎµ¦½º)");
-		System.out.println(numbers.get(2));
+    System.out.println("\nget(ì¸ë±ìŠ¤)");
+    System.out.println(numbers.get(2));
 
-		System.out.println("\nsize()");
-		System.out.println(numbers.size());
+    System.out.println("\nsize()");
+    System.out.println(numbers.size());
 
-		System.out.println("\nindexOf()");
-		System.out.println(numbers.indexOf(30));
+    System.out.println("\nindexOf()");
+    System.out.println(numbers.indexOf(30));
 
-		Iterator it = numbers.iterator();
-		System.out.println("\niterator");
-		while (it.hasNext()) {
-			int value = (int) it.next();
-			if (value == 30) {
-				it.remove();
-			}
-			System.out.println(value);
-		}
-		System.out.println(numbers);
+    Iterator it = numbers.iterator();
+    System.out.println("\niterator");
+    while (it.hasNext()) {
+      int value = (int) it.next();
+      if (value == 30) {
+        it.remove();
+      }
+      System.out.println(value);
+    }
+    System.out.println(numbers);
 
-		System.out.println("\nfor each");
-		for (int value : numbers) {
-			System.out.println(value);
-		}
-		System.out.println("\nfor");
-		for (int i = 0; i < numbers.size(); i++) {
-			System.out.println(numbers.get(i));
-		}
+    System.out.println("\nfor each");
+    for (int value : numbers) {
+      System.out.println(value);
+    }
+    System.out.println("\nfor");
+    for (int i = 0; i < numbers.size(); i++) {
+      System.out.println(numbers.get(i));
+    }
 
-	}
+  }
 
 }
